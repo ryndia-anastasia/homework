a2731d4dd6442f7b5086f9850978a5a73ab4bb15
ryndia-anastasia <ryndia.anastasia@globaldev.tech>
2023-10-10 21:06:01 +0300

    hw

---
+++ hw2/hw5.js
@@ -0,0 +1,35 @@
+let a = prompt("Вкажіть Bаш вік");
+
+let age = "i"
+let count = age
+let result = ""
+if (count >=10 && count <=20) {
+    result = "років"
+} else {
+    count = age % 10
+
+    if(count === 1) {
+        result = "рік"}
+    else if (count >=2 && count<=4){
+        result = "роки"
+    }
+    else if (count >=2 && count<=4){
+        result = "роки"
+        }
+    else if (age < 0) {
+        console.log(Error)
+    }
+    else if (age = isNaN) {
+        console.log(Error)
+    }
+    else {result = "років"}
+    console.log (age + " " + result);}
+
+
+
+
+
+
+
+
+
