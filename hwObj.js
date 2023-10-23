4912c373e336de9fbf15b965202a7d00e4db8205
ryndia-anastasia <ryndia.anastasia@globaldev.tech>
2023-10-23 19:53:26 +0300

    hw5

---
+++ hwObj.js
@@ -0,0 +1,38 @@
+/*var cat = {
+    name: "Tigra",
+    age: 6,
+    gender: 'female',
+    color: "grey",
+
+    getInfo () {
+    return cat;
+} }
+//console.log(cat.getInfo())
+
+cat.hobby = "Sleep";
+
+    console.log(cat.getInfo()) */
+
+/*var services = {
+    "стрижка": 70,
+    "гоління": 80,
+    "миття голови": 100,
+}
+services['Розбити скло'] = 200;
+services["Фарбування"] = 400;
+let price = 0;
+for (let key in services) {
+    if (typeof services[key] === 'number')
+        price += services[key];
+}
+
+let maxPrice = Math.max.apply(null, Object.values(services));
+
+let minPrice = Math.min.apply(null, Object.values(services));
+
+console.log ("загальна вартість наданих послуг" + " " + price + "грн",'\n',
+    "мінімальна ціна за послуги" + " " + minPrice + "грн",'\n',
+    "максимальна ціна за послуги" + " " + maxPrice + "грн");
+
+*/
+
